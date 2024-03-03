8e42b7618c3c8500c8fb745573420acb25b2d74c
Lezzio <allan.guigal0@gmail.com>
2024-03-03 03:05:40 +0100

    feat: init monorepo and packages

---
+++ apps/chatbot/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Cairo chatbot",
+  description: "Discuss and learn about Cairo with our AI-powered chatbot",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ packages/ia/src/index.ts
@@ -0,0 +1,1 @@
+console.log("IA file")
