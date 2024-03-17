b1f8a92e7ecb642b3416d14f47477502f22f5de8
Mathieu <60658558+enitrat@users.noreply.github.com>
2024-03-17 15:32:41 +0100

    Revert "feat/chat ui (#10)"
    This reverts commit 3468d36756a00e15c29f5e0748ca3b614c9b04cd.

--- packages/ai/features/augmentedPromptChat/augmentedPrompt.usecase.ts
+++ packages/ai/features/augmentedPromptChat/augmentedPrompt.usecase.ts
@@ -1,31 +1,13 @@
-import { ChatMessage } from "./types";
+import { ChatHistory } from "./types";
 import { ragChatAgent } from "./ragChain";
-import buildChatHistory from "./buildChatHistory";
-import { ValidateKeyUtils } from "../../utils/validateKey.utils";
-import { InvalidKeyError } from "../../error/invalidKeyError.error";
-import { HttpResponseOutputParser } from "langchain/output_parsers";
-import { IterableReadableStream } from "@langchain/core/utils/stream";
 
 export async function augmentedPromptChatUseCase(
-  messages: ChatMessage[], 
-  previewToken: string
-) : Promise<IterableReadableStream<Uint8Array>> {
+  prompt: string,
+  chatHistory: ChatHistory,
+) {
   console.log("Running augmented prompt chat use case...");
-
-  if (!ValidateKeyUtils.isKeyValid(previewToken)) {
-    throw new InvalidKeyError('Invalid Access Key');
-  }
-
-  let prompt = ''
-  if (messages && messages.length > 0) {
-    prompt = messages![messages.length - 1]!.content;
-    messages.pop();
-  }
-
-  const parser = new HttpResponseOutputParser();
-
-  return await ragChatAgent.pipe(parser).stream({
+  return await ragChatAgent.invoke({
     question: prompt,
-    chat_history: buildChatHistory(messages),
+    chat_history: chatHistory,
   });
 }
