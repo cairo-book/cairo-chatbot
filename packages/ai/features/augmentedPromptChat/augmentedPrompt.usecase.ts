ea242ff365ed1a574fb676df5dba78888a73c8a8
Benoit DELEGLISE <b.deleglise@groupeonepoint.com>
2024-03-17 12:24:48 +0100

    feat/chat back and front connection

--- packages/ai/features/augmentedPromptChat/augmentedPrompt.usecase.ts
+++ packages/ai/features/augmentedPromptChat/augmentedPrompt.usecase.ts
@@ -1,13 +1,31 @@
-import { ChatHistory } from "./types";
+import { ChatMessage } from "./types";
 import { ragChatAgent } from "./ragChain";
+import buildChatHistory from "./buildChatHistory";
+import { ValidateKeyUtils } from "../../utils/validateKey.utils";
+import { InvalidKeyError } from "../../error/invalidKeyError.error";
+import { HttpResponseOutputParser } from "langchain/output_parsers";
+import { IterableReadableStream } from "@langchain/core/utils/stream";
 
 export async function augmentedPromptChatUseCase(
-  prompt: string,
-  chatHistory: ChatHistory,
-) {
+  messages: ChatMessage[], 
+  previewToken: string
+) : Promise<IterableReadableStream<Uint8Array>> {
   console.log("Running augmented prompt chat use case...");
-  return await ragChatAgent.invoke({
+
+  if (!ValidateKeyUtils.isKeyValid(previewToken)) {
+    throw new InvalidKeyError('Invalid Access Key');
+  }
+
+  let prompt = ''
+  if (messages && messages.length > 0) {
+    prompt = messages![messages.length - 1]!.content;
+    messages.pop();
+  }
+
+  const parser = new HttpResponseOutputParser();
+
+  return await ragChatAgent.pipe(parser).stream({
     question: prompt,
-    chat_history: chatHistory,
+    chat_history: buildChatHistory(messages),
   });
 }
