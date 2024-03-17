8152f5a907f8529bdee0bc838e8d56369678d366
Benoit DELEGLISE <b.deleglise@groupeonepoint.com>
2024-03-17 12:24:48 +0100

    feat/chat back and front connection

--- apps/chatbot/app/api/chat/route.ts
+++ apps/chatbot/app/api/chat/route.ts
@@ -1,4 +1,4 @@
-import { augmentedPromptUsecase } from "@repo/ai/features/augmentedPrompt/augmentedPrompt.usecase"
+import { augmentedPromptChatUseCase } from "@repo/ai/features/augmentedPromptChat/augmentedPrompt.usecase"
 import { StreamingTextResponse } from 'ai'
 import { InvalidKeyError } from '@repo/ai/error/invalidKeyError.error'
 
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
     const json = await req.json();
     const { messages, previewToken } = json;
     
-    const stream = await augmentedPromptUsecase({messages, previewToken});
+    const stream = await augmentedPromptChatUseCase(messages, previewToken);
     return new StreamingTextResponse(stream);
   } catch (error) {
     console.error(error);
