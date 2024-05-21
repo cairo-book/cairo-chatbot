3075dd62e49dec62515d9395606949d0e0445411
Fricoben <benjamin.flores.pro@gmail.com>
2024-05-21 10:23:44 +0100

    feat: add analytics

--- apps/chatbot/components/provider.tsx
+++ apps/chatbot/components/provider.tsx
@@ -1,17 +1,33 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
-import { SidebarProvider } from '../lib/hooks/use-sidebar'
-import { TooltipProvider } from './ui/tooltip'
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
+import { SidebarProvider } from "../lib/hooks/use-sidebar";
+import { TooltipProvider } from "./ui/tooltip";
+import { PostHogProvider } from "posthog-js/react";
+import posthog from "posthog-js";
+
+// Traffic measures
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+    api_host: "https://app.posthog.com",
+    session_recording: {
+      recordCrossOriginIframes: true,
+    },
+    capture_pageleave: false,
+  });
+  (window as any).posthog = posthog;
+}
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <SidebarProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-      </SidebarProvider>
+      <PostHogProvider client={posthog}>
+        <SidebarProvider>
+          <TooltipProvider>{children}</TooltipProvider>
+        </SidebarProvider>
+      </PostHogProvider>
     </NextThemesProvider>
-  )
+  );
 }
