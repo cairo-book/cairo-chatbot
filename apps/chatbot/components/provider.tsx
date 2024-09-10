68dc1c47e8a107d0e8c038851b442040ef41db99
Fricoben <benjamin.flores.pro@gmail.com>
2024-09-10 09:44:01 -0300

    ref:migrate to new agent

--- apps/chatbot/components/provider.tsx
+++ apps/chatbot/components/provider.tsx
@@ -5,29 +5,13 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { SidebarProvider } from "../lib/hooks/use-sidebar";
 import { TooltipProvider } from "./ui/tooltip";
-import { PostHogProvider } from "posthog-js/react";
-import posthog from "posthog-js";
-
-// Traffic measures
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: "https://app.posthog.com",
-    session_recording: {
-      recordCrossOriginIframes: true,
-    },
-    capture_pageleave: false,
-  });
-  (window as any).posthog = posthog;
-}
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <PostHogProvider client={posthog}>
-        <SidebarProvider>
-          <TooltipProvider>{children}</TooltipProvider>
-        </SidebarProvider>
-      </PostHogProvider>
+      <SidebarProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </SidebarProvider>
     </NextThemesProvider>
   );
 }
